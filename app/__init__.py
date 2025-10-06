5e45fb8ef747a7c78dd2d3dfadc58a79696d5ae9
Ismail Yunusa <ismailyunusa38@gmail.com>
2025-10-06 22:11:04 +0100

    Fix dublicate create_app functions

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,15 @@
 from flask import Flask
-from .extensions import db
+from .extensions import db,  login_manager
 
 from .routes import main
 from .auth import auth   # 👈 import auth blueprint
 
-def create_app():
+def create_app(static_folder="static", template_folder="templates"):
     app = Flask(__name__, instance_relative_config=True,
                 template_folder="../templates",
                 static_folder="../static")
     app.config.from_object("config.Config")
 
-    from .extensions import db, login_manager
     db.init_app(app)
     login_manager.init_app(app)
 

95df7a1b79567c07948e2ceeef3f50d882c54e4c
Ismail Yunusa <ismailyunusa38@gmail.com>
2025-10-06 22:50:33 +0100

    Fix Gunicorn app reference

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from app import create_app
 
-app = create_app(
-    static_folder='static',
-    template_folder='templates'
-)
+app = create_app()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == "_main_":
+    app.run()
